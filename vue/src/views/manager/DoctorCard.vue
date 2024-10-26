1ee14797bfdca878df326cdb3da87d06533cf6c9
LuHao <fenlyin0420@gmail.com>
2024-10-26 12:02:11 +0800

    插件格式化

--- vue/src/views/manager/DoctorCard.vue
+++ vue/src/views/manager/DoctorCard.vue
@@ -5,24 +5,43 @@
       <!-- 选择医院 -->
       <label for="hospital">医院:</label>
       <el-select id="hospital" placeholder="请选择医院" v-model="hospitalId">
-        <el-option v-for="item in hospitalList" :key="item.id" :label="item.hospitalName" :value="item.id"></el-option>
+        <el-option
+          v-for="item in hospitalList"
+          :key="item.id"
+          :label="item.hospitalName"
+          :value="item.id"
+        ></el-option>
       </el-select>
 
       <!-- 选择科室 -->
       <label for="departmentSelect">科室:</label>
       <el-select id="departmentSelect" placeholder="请选择科室" v-model="departmentId">
-        <el-option v-for="item in departmentList" :key="item.id" :label="item.name" :value="item.id"></el-option>
+        <el-option
+          v-for="item in departmentList"
+          :key="item.id"
+          :label="item.name"
+          :value="item.id"
+        ></el-option>
       </el-select>
 
       <!-- 选择日期 -->
       <label for="selectedDate">日期:</label>
       <el-select id="selectedDate" placeholder="请选择日期" v-model="selectedDate">
-        <el-option v-for="item in timestamp" :key="item.id" :label="item.label" :value="item.id"></el-option>
+        <el-option
+          v-for="item in timestamp"
+          :key="item.id"
+          :label="item.label"
+          :value="item.id"
+        ></el-option>
       </el-select>
 
       <div class="searchBtn">
-        <el-button type="info" plain style="margin-left: 10px" @click="query(1)">查询</el-button>
-        <el-button type="warning" plain style="margin-left: 10px" @click="reset">重置</el-button>
+        <el-button type="info" plain style="margin-left: 10px" @click="query(1)"
+          >查询</el-button
+        >
+        <el-button type="warning" plain style="margin-left: 10px" @click="reset"
+          >重置</el-button
+        >
       </div>
     </div>
 
@@ -31,25 +50,53 @@
       <el-row :gutter="20">
         <el-col :span="6" v-for="item in tableData" style="margin-bottom: 20px">
           <div style="text-align: center; background-color: #ecf8fd" class="card">
-            <img :src="item.avatar" alt="" style="width: 100px; height: 100px; border-radius: 50%">
+            <img
+              :src="item.avatar"
+              alt=""
+              style="width: 100px; height: 100px; border-radius: 50%"
+            />
             <div style="font-weight: 550; margin-top: 10px">
-              医院 <span style="color: #383535; margin-left: 5px; font-weight: 500">{{ item.hospitalName }}</span>
+              医院
+              <span style="color: #383535; margin-left: 5px; font-weight: 500">{{
+                item.hospitalName
+              }}</span>
             </div>
             <div style="font-weight: 550; margin-top: 10px">
-              {{ item.name }} <span style="color: #383535; margin-left: 5px; font-weight: 500">{{ item.departmentName
-                }}</span>
+              {{ item.name }}
+              <span style="color: #383535; margin-left: 5px; font-weight: 500">{{
+                item.departmentName
+              }}</span>
             </div>
             <div
-              style="margin-top: 20px; color: #353523; padding: 0 10px; text-align: left; overflow: hidden;text-overflow: ellipsis;display: -webkit-box;-webkit-box-orient: vertical;-webkit-line-clamp: 4;">
+              style="
+                margin-top: 20px;
+                color: #353523;
+                padding: 0 10px;
+                text-align: left;
+                overflow: hidden;
+                text-overflow: ellipsis;
+                display: -webkit-box;
+                -webkit-box-orient: vertical;
+                -webkit-line-clamp: 4;
+              "
+            >
               简介：{{ item.description }}
             </div>
             <div style="margin-top: 15px">
-              挂号费：<span style="color: red; font-weight: 550; margin-right: 20px">￥{{ item.price }}</span> 剩余：{{ item.num
-              }}
+              挂号费：<span style="color: red; font-weight: 550; margin-right: 20px"
+                >￥{{ item.price }}</span
+              >
+              剩余：{{ item.num }}
             </div>
             <!-- 挂号按钮 -->
             <div style="margin-top: 15px">
-              <el-button type="primary" size="mini" :disabled="disabled" @click="showDialog(item)">挂号</el-button>
+              <el-button
+                type="primary"
+                size="mini"
+                :disabled="disabled"
+                @click="showDialog(item)"
+                >挂号</el-button
+              >
               <el-dialog title="确认订单" :visible.sync="dialogVisible" width="30%">
                 <Payment :form="dialogDate"></Payment>
 
@@ -65,8 +112,15 @@
 
       <!-- 选择分页区 -->
       <div class="pagination">
-        <el-pagination background @current-change="handleCurrentChange" :current-page="pageNum"
-          :page-sizes="[5, 10, 20]" :page-size="pageSize" layout="total, prev, pager, next" :total="total">
+        <el-pagination
+          background
+          @current-change="handleCurrentChange"
+          :current-page="pageNum"
+          :page-sizes="[5, 10, 20]"
+          :page-size="pageSize"
+          layout="total, prev, pager, next"
+          :total="total"
+        >
         </el-pagination>
       </div>
     </div>
@@ -83,15 +137,15 @@ export default {
   data() {
     return {
       /** API: /doctor/selectpage2 */
-      tableData: [],  
+      tableData: [],
       /** API: /plan/selectAll */
       planList: [],
       /** API: /hospital/selectAll */
       hospitalList: [],
       /** API: /department/selectAll */
       departmentList: [],
-      pageNum: 1,   // 当前的页码
-      pageSize: 10,  // 每页显示的个数
+      pageNum: 1, // 当前的页码
+      pageSize: 10, // 每页显示的个数
       total: 0,
       /** 用户选择的医院 */
       hospitalId: null,
@@ -102,12 +156,12 @@ export default {
       fromVisible: false,
       form: {},
       /** user entity, an object */
-      user: JSON.parse(localStorage.getItem('xm-user') || '{}'),
+      user: JSON.parse(localStorage.getItem("xm-user") || "{}"),
       rules: {},
       ids: [],
       disabled: false,
       /** 顶部搜索区域 tab 默认项*/
-      activeName: 'hospital',
+      activeName: "hospital",
       /** 支付界面可视性 */
       dialogVisible: false,
       /** 向支付界面传递的数据 */
@@ -120,7 +174,7 @@ export default {
        * }
        */
       timestamp: [],
-    }
+    };
   },
   created() {
     /** 生成七天日期数据 */
@@ -132,27 +186,28 @@ export default {
       "星期四",
       "星期五",
       "星期六",
-    ]
-    let startDate = new Date()
-    let dateTemp = new Date()
+    ];
+    let startDate = new Date();
+    let dateTemp = new Date();
     for (let i = 0; i < 7; i++) {
-      dateTemp.setDate(startDate.getDate() + i)
-      let week = dateTemp.getDay()
-      let label = weekIndex[week] + '🔹️' + dateTemp.toISOString().split('T')[0]
-      this.timestamp.push({ id: i, label, value: dateTemp.toISOString().split('T')[0] })
+      dateTemp.setDate(startDate.getDate() + i);
+      let week = dateTemp.getDay();
+      let label = weekIndex[week] + "🔹️" + dateTemp.toISOString().split("T")[0];
+      this.timestamp.push({ id: i, label, value: dateTemp.toISOString().split("T")[0] });
     }
 
-    Promise.all([this.loadDepartments(), this.loadHospitals()]).then(() => {
-      this.query(1)
-    }).catch(err => {
-      this.$message.error(err)
-    })
+    Promise.all([this.loadDepartments(), this.loadHospitals()])
+      .then(() => {
+        this.query(1);
+      })
+      .catch((err) => {
+        this.$message.error(err);
+      });
 
     // 初始化过滤日期
-    this.selectedDate = this.timestamp[0]?.value
+    this.selectedDate = this.timestamp[0]?.value;
   },
-  mounted() {
-  },
+  mounted() {},
   methods: {
     /**
      * 挂号操作
@@ -161,103 +216,114 @@ export default {
      */
     reserve(item) {
       this.dialogVisible = false;
-      if (this.user.role !== 'USER') {
-        this.$message.warning('您的角色不支持挂号操作')
-        return
+      if (this.user.role !== "USER") {
+        this.$message.warning("您的角色不支持挂号操作");
+        return;
       }
       if (this.user.account < item.price) {
-        this.$message.error("您的账余额不足")
-        return -1
+        this.$message.error("您的账余额不足");
+        return -1;
       }
       let reserveBody = {
         userId: this.user.id,
         doctorId: item.id,
         hospitalId: item.hospitalId,
-        time: item.selectedDate
-      }
+        time: item.selectedDate,
+      };
       let planBody = {
         hospitalId: item.hospitalId,
         doctorId: item.id,
         date: item.selectedDate,
-      }
+      };
 
       /** 修改挂号记录表 reserve */
-      this.$request.post('/reserve/add', reserveBody).then(res => {
-        if (res.code !== '200') {
-          this.$message.error(res.msg)
-          return -1
+      this.$request.post("/reserve/add", reserveBody).then((res) => {
+        if (res.code !== "200") {
+          this.$message.error(res.msg);
+          return -1;
         }
-      })
+      });
       /** 修改医生排班表中的剩余挂号数量 plan */
-      this.$request.post('/plan/updateNum', planBody).then(res => {
-        if (res.code !== '200') {
-          this.$message.error("update num failed")
-          return -1
+      this.$request.post("/plan/updateNum", planBody).then((res) => {
+        if (res.code !== "200") {
+          this.$message.error("update num failed");
+          return -1;
         }
-      })
+      });
       /** 修改账户余额 user */
-      this.$request.put('/user/update', { id: this.user.id, account: this.user.account - item.price }).then(res => {
-        if (res.code === '200') {
-          this.$message.success("挂号成功")
-          this.query(1)
-        } else {
-          this.$message.error(res.data)
-        }
-      })
+      this.$request
+        .put("/user/update", {
+          id: this.user.id,
+          account: this.user.account - item.price,
+        })
+        .then((res) => {
+          if (res.code === "200") {
+            this.$message.success("挂号成功");
+            this.query(1);
+          } else {
+            this.$message.error(res.data);
+          }
+        });
     },
 
     showDialog(item) {
-      this.dialogVisible = true
+      this.dialogVisible = true;
       this.dialogDate = {
         department: item.departmentName,
         doctor: item.name,
         price: item.price,
-        date: item.selectedDate
-      }
+        date: item.selectedDate,
+      };
     },
     /**
-     * 获取指定排班排班 
+     * 获取指定排班排班
      */
     loadPlan(hospitalId, departmentId, selectedDate) {
       return new Promise((resolve, reject) => {
         body = {
           hospitalId,
           departmentId,
-          date: selectedDate
-        }
-        this.$request.get('/plan/selectAll').then(res => {
-          if (res.code === '200') {
-            this.planList = res.data;
-            resolve();
-          } else {
-            this.$message.error(res.msg);
-            reject();
-          }
-        }).catch(err => {
-          this.$message.error('加载计划数据失败');
-          reject(err);
-        });
+          date: selectedDate,
+        };
+        this.$request
+          .get("/plan/selectAll")
+          .then((res) => {
+            if (res.code === "200") {
+              this.planList = res.data;
+              resolve();
+            } else {
+              this.$message.error(res.msg);
+              reject();
+            }
+          })
+          .catch((err) => {
+            this.$message.error("加载计划数据失败");
+            reject(err);
+          });
       });
-    },    
+    },
     /**
      * 获取所有的医院
      */
     loadHospitals() {
       return new Promise((resolve, reject) => {
-        this.$request.get('/hospital/selectAll').then(res => {
-          if (res.code === '200') {
-            this.hospitalList = res.data;
-            // 初始化医院
-            this.hospitalId = this.hospitalList[0]?.id
-            resolve();
-          } else {
-            this.$message.error(res.msg);
-            reject();
-          }
-        }).catch(err => {
-          this.$message.error('加载医院数据失败');
-          reject(err);
-        });
+        this.$request
+          .get("/hospital/selectAll")
+          .then((res) => {
+            if (res.code === "200") {
+              this.hospitalList = res.data;
+              // 初始化医院
+              this.hospitalId = this.hospitalList[0]?.id;
+              resolve();
+            } else {
+              this.$message.error(res.msg);
+              reject();
+            }
+          })
+          .catch((err) => {
+            this.$message.error("加载医院数据失败");
+            reject(err);
+          });
       });
     },
     /**
@@ -265,20 +331,23 @@ export default {
      */
     loadDepartments() {
       return new Promise((resolve, reject) => {
-        this.$request.get('/department/selectAll').then(res => {
-          if (res.code === '200') {
-            this.departmentList = res.data;
-            // 初始化科室
-            this.departmentId = this.departmentList[0]?.id
-            resolve();
-          } else {
-            this.$message.error(res.msg);
-            reject();
-          }
-        }).catch(err => {
-          this.$message.error('加载科室信息失败')
-          reject(err);
-        });
+        this.$request
+          .get("/department/selectAll")
+          .then((res) => {
+            if (res.code === "200") {
+              this.departmentList = res.data;
+              // 初始化科室
+              this.departmentId = this.departmentList[0]?.id;
+              resolve();
+            } else {
+              this.$message.error(res.msg);
+              reject();
+            }
+          })
+          .catch((err) => {
+            this.$message.error("加载科室信息失败");
+            reject(err);
+          });
       });
     },
     /**
@@ -289,35 +358,37 @@ export default {
      * @param pageNum 页号
      */
     query(pageNum) {
-      if (pageNum) this.pageNum = pageNum
-      this.$request.get('/doctor/selectPage2', {
-        params: {
-          pageNum: this.pageNum,
-          pageSize: this.pageSize,
-          departmentId: this.departmentId,
-          hospitalId: this.hospitalId,
-          selectedDate: this.selectedDate
-        }
-      }).then(res => {
-        if (res.code === '200') {
-          this.tableData = res.data?.list
-          this.total = res.data?.total
-        } else {
-          this.$message.error(res.msg)
-        }
-      })
+      if (pageNum) this.pageNum = pageNum;
+      this.$request
+        .get("/doctor/selectPage2", {
+          params: {
+            pageNum: this.pageNum,
+            pageSize: this.pageSize,
+            departmentId: this.departmentId,
+            hospitalId: this.hospitalId,
+            selectedDate: this.selectedDate,
+          },
+        })
+        .then((res) => {
+          if (res.code === "200") {
+            this.tableData = res.data?.list;
+            this.total = res.data?.total;
+          } else {
+            this.$message.error(res.msg);
+          }
+        });
     },
     reset() {
-      this.departmentId = this.planList[0]?.id
-      this.hospitalId = this.hospitalList[0]?.id
-      this.selectedDate = this.timestamp[0]?.value
-      this.query(1)
+      this.departmentId = this.planList[0]?.id;
+      this.hospitalId = this.hospitalList[0]?.id;
+      this.selectedDate = this.timestamp[0]?.value;
+      this.query(1);
     },
     handleCurrentChange(pageNum) {
-      this.query(pageNum)
+      this.query(pageNum);
     },
-  }
-}
+  },
+};
 </script>
 
 <style scoped>
