4e21d190218dbc44101701e20ce1610ac021e004
Confident_topShen <939793038@qq.com>
2024-11-11 20:20:56 +0800

    修正了出现多个医生的bug，完善了管理员和护士端所有功能

--- vue/src/views/manager/Nurse/NursePlan.vue
+++ vue/src/views/manager/Nurse/NursePlan.vue
@@ -179,7 +179,8 @@ export default {
   },
   methods: {
     loadNurse() {
-      let id = this.user.hospitalId; // 各医院管理员只能管理自己医院的排班，所以是selectByhospitalId
+      //这里帮我debug一下
+      let id = this.user.hospitalId; // 各医院管理员只能管理自己医院的排班，所以是selectByHospitalId
       this.$request.get("/nurse/selectByH/" + id).then((res) => {
         if (res.code === "200") {
           this.nurseData = res.data;
