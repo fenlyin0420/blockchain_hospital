b1fe4e209444dc36f861df325a4e3ca6e35cda0f
LuHao <fenlyin0420@gmail.com>
2024-11-25 18:46:32 +0800

    转诊优化

--- vue/src/views/records/ReferralApplication.vue
+++ vue/src/views/records/ReferralApplication.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <div class="form-container">
+
       <div class="left-form">
         <div class="label">转诊患者</div>
         <el-select v-model="caseInfo" placeholder="请选择患者" style="width:80%; margin-bottom: 20px;">
@@ -10,30 +11,34 @@
         </el-select>
 
         <div class="label">转出信息</div>
-        <el-input placeholder="转出医院" v-model="caseInfo.hospitalName" :readonly="true" clearable style="width:80%;"></el-input>
-        <el-input placeholder="转出医生" v-model="caseInfo.doctorName" :readonly="true" clearable style="width:80%;"></el-input>
+        <el-input placeholder="转出医院" v-model="caseInfo.hospitalName" :readonly="true" clearable
+          style="width:80%;"></el-input>
+        <el-input placeholder="转出医生" v-model="caseInfo.doctorName" :readonly="true" clearable
+          style="width:80%;"></el-input>
         <div class="label">转入信息</div>
-        <el-select v-model="transferInHospital" placeholder="请选择医院" @change="loadByDoctor()" style="width: 80%;margin-bottom: 20px">
+        <el-select v-model="transferInHospital" placeholder="请选择医院" @change="loadByDoctor()"
+          style="width: 80%;margin-bottom: 20px">
           <div v-for="item in infByHospital">
             <el-option :label="item.hospitalName" :value="item.id"></el-option>
           </div>
         </el-select>
-        
-        <el-input type="textarea" placeholder="转院理由" v-model="transferReason" clearable :rows="4" style="width:80%;"></el-input><br/>
-        <el-button type="primary" style="margin-top: 10px; position:absolute; right:50%" @click="confirmTransfer">确定</el-button>
+
+        <el-autocomplete type="textarea" v-model="transferReason" :fetch-suggestions="transferReasonComplete" clearable
+          placeholder="请输入转诊原因" @select="handleSelect" style="width: 80%;display:block" />
+        <el-button type="primary" style="margin-top: 10px; position: relative; left:400px"
+          @click="confirmTransfer">确定</el-button>
       </div>
 
       <div class="right-form">
         <el-form label-width="100px" style="margin-top: 20px;">
           <div class="label">诊断结果</div>
           <el-form-item>
-            <el-input  :autosize="{ minRows: 4, maxRows: 4 }"
-              v-model="advice">
+            <el-input :autosize="{ minRows: 4, maxRows: 4 }" v-model="advice">
             </el-input>
           </el-form-item>
           <div class="label">药品信息</div>
           <el-form-item>
-            <el-table :data="drug" style="width: 100%"  border>
+            <el-table :data="drug" style="width: 100%" border>
               <el-table-column prop="name" label="药品名称"></el-table-column>
               <el-table-column prop="dose" label="数量"></el-table-column>
               <el-table-column prop="frequency" label="用法用量"></el-table-column>
@@ -43,7 +48,7 @@
       </div>
     </div>
 
-    
+
   </div>
 </template>
 
@@ -53,7 +58,7 @@ export default {
   data() {
     return {
       user: JSON.parse(localStorage.getItem('xm-user') || '{}'),
-      name:"",
+      name: "",
       transferOutHospital: '',
       transferOutDoctor: '',
       transferOutTime: null,
@@ -61,29 +66,35 @@ export default {
       transferInDoctor: '',
       transferInTime: null,
       transferReason: '',
-      caseInfo:[],
-      tableData:[],
-      infByHospital:[],
-      infByDoctor:[],
-      information:{},
-      drug:[
-        {        
-        name: "999感冒灵颗粒",
-        dose: 3,
-        frequency: "一日3次"
+      caseInfo: [],
+      tableData: [],
+      infByHospital: [],
+      infByDoctor: [],
+      information: {},
+      drug: [
+        {
+          name: "999感冒灵颗粒",
+          dose: 3,
+          frequency: "一日3次"
         },
-        {        
-        name: "盐酸左氧氟沙星片",
-        dose: 3,
-        frequency: "一日3次"
+        {
+          name: "盐酸左氧氟沙星片",
+          dose: 3,
+          frequency: "一日3次"
         },
-        {        
-        name: "健胃消食片",
-        dose: 3,
-        frequency: "一日3次"
+        {
+          name: "健胃消食片",
+          dose: 3,
+          frequency: "一日3次"
         },
       ],
-      advice: "多喝水"
+      advice: "多喝水",
+      transferReason: '',
+      suggestions: [
+        { value: "由于我院当前技术水平、设备条件，不能确诊或治疗条件有限的患者。" },
+        { value: "患者病情稳定。" },
+        { value: "患者及家属要求转诊转院者。" }
+      ]
     }
   },
   created() {
@@ -92,10 +103,10 @@ export default {
     //this.loadByDoctor()
   },
   methods: {
-    loadByUser(){
-      this.$request.get('/record/selectAll',{ // 只能再就诊记录中选择要转诊的患者
-        params:{
-          doctorId:this.user.id
+    loadByUser() {
+      this.$request.get('/record/selectAll', { // 只能再就诊记录中选择要转诊的患者
+        params: {
+          doctorId: this.user.id
         }
       }).then(res => {
         this.tableData = res.data
@@ -103,8 +114,8 @@ export default {
       })
     },
     loadByDoctor() {
-      let id=this.transferInHospital
-      this.$request.get('/doctor/selectByH/'+id).then(res => {
+      let id = this.transferInHospital
+      this.$request.get('/doctor/selectByH/' + id).then(res => {
         if (res.code === '200') {
           this.infByDoctor = res.data
         } else {
@@ -112,21 +123,21 @@ export default {
         }
       })
     },
-    loadByHospital(){
-      this.$request.get('/hospital/selectAll').then(res=>{
-        this.infByHospital=res.data
+    loadByHospital() {
+      this.$request.get('/hospital/selectAll').then(res => {
+        this.infByHospital = res.data
       })
     },
     /**
      * 转诊申请
      */
     confirmTransfer() {
-      this.information.userId=this.caseInfo.userId
-      this.information.outHospitalId=this.caseInfo.hospitalId
-      this.information.outDoctorId=this.caseInfo.doctorId
-      this.information.inHospitalId=this.transferInHospital
+      this.information.userId = this.caseInfo.userId
+      this.information.outHospitalId = this.caseInfo.hospitalId
+      this.information.outDoctorId = this.caseInfo.doctorId
+      this.information.inHospitalId = this.transferInHospital
       // this.information.inDoctorId=this.transferInDoctor // 医生由管理员分配
-      this.information.why=this.transferReason
+      this.information.why = this.transferReason
       // this.information.result="待接收"
       let data = JSON.parse(JSON.stringify(this.information))
       this.$request.post('/referal/add', data).then(res => {
@@ -147,6 +158,14 @@ export default {
           this.$message.error(res.msg)
         }
       })
+    },
+    transferReasonComplete(queryString, cb) {
+      let results = this.suggestions.filter(item => item.value.toLowerCase().indexOf(queryString.toLowerCase()) !== -1);
+      cb(results);
+    },
+    handleSelect(item) {
+      // 这里可以添加当选中某个联想数据后的具体处理逻辑，比如赋值给其他变量等
+      this.transferReason = item.value;
     }
   }
 }
@@ -158,19 +177,19 @@ export default {
   justify-content: space-between;
 }
 
-.left-form, .right-form {
+.left-form,
+.right-form {
   flex: 1;
   margin-right: 20px;
 }
-
-
 
 .label {
   margin-bottom: 10px;
   font-weight: bold;
 }
 
-.el-input, .el-date-picker {
+.el-input,
+.el-date-picker {
   margin-bottom: 20px;
 }
 
