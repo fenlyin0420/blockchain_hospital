e8d673fa13bbb721d72e782aa9b4b653656e7381
LuHao <fenlyin0420@gmail.com>
2025-03-27 17:25:14 +0800

    添加浮动角色组件，支持拖动和点击跳转功能，更新App.vue以集成该组件。

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,5 +1,21 @@
 <template>
   <div id="app">
     <router-view/>
+    <floating-character 
+      :target-route="'/referral/out'"
+      :size="60"
+      :drag-threshold="5"
+    />
   </div>
 </template>
+
+<script>
+import FloatingCharacter from '@/views/component/FloatingCharacter.vue'
+
+export default {
+  name: 'App',
+  components: {
+    FloatingCharacter
+  }
+}
+</script>
